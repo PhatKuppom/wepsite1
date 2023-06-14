ff595756675dbe2f3bf1af8fa4022c81a730bcef
PhatKuppom <phat777x2@gmail.com>
2023-06-14 15:31:41 +0700

    first commit

---
+++ phat.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="style.css"/>
+
+<?php
+$name = "Naphat inprom";
+  echo " <h1> Welcome!!! </h1>  ";
+  echo "  <h2> $name  </h2>  ";
+  echo " <h2> <img src='images/4.jpg' class='img1 <h2>' />  <br/>";
+?>
+<p class="uuu">
+idk
+</p>
